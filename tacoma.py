3029a1382d9d5ea0c1cfe6dd0d3a07c57608940f
Will Rooney <william.rooney@ucdenver.edu>
2016-12-13 15:11:19 -0700

    Initial Commit

--- tacoma.py
+++ tacoma.py
@@ -14,47 +14,47 @@ import matplotlib.pyplot as plt
 import math
 
 class tacoma:
-    def __init__(self, inter, ic, n, p):
-    	self.debug = True
+	def __init__(self, inter, ic, n, p):
+		self.debug = True
 
-    	self.len = 6
-    	self.a = 0.2
-    	self.w = 80
-    	self.omega = 2*np.pi*38/60
-    	self.inter = inter
-    	self.ic = ic
-    	self.n = n
-    	self.p = p
-    	self.h = (inter[1] - inter[0]) / self.n
+		self.len = 6
+		self.a = 0.2
+		self.w = 80
+		self.omega = 2*np.pi*38/60
+		self.inter = inter
+		self.ic = ic
+		self.n = n
+		self.p = p
+		self.h = (inter[1] - inter[0]) / self.n
 
-    	yInit = []
-    	self.y = []
-    	for i in ic:
-    		yInit.append(i)
-    	if self.debug:
-    		print "yInit:",yInit
-    	self.y.append(yInit)
+		yInit = []
+		self.y = []
+		for i in ic:
+			yInit.append(i)
+		if self.debug:
+			print "yInit:",yInit
+		self.y.append(yInit)
 
-    	# TODO: Initialize self.y dimensions
-    	# TODO: Initialize self.t & dimensions with 'inter'
+		# TODO: Initialize self.y dimensions
+		# TODO: Initialize self.t & dimensions with 'inter'
 
-    def setLen(self, len):
-    	self.len = len
+	def setLen(self, len):
+		self.len = len
 
-    def setA(self, a):
-    	self.a = a
+	def setA(self, a):
+		self.a = a
 
-    def setW(self, w):
-    	self.w = w
+	def setW(self, w):
+		self.w = w
 
-    def setOmega(self, omega):
-    	self.omega = omega
+	def setOmega(self, omega):
+		self.omega = omega
 
-    def run(self):
-    	"""
-    	Matlab Code:
+	def run(self):
+		"""
+		Matlab Code:
 
-    	t(1)=inter(1);
+		t(1)=inter(1);
 		set(gca,'XLim',[-8 8],'YLim',[-8 8], ...
 		   'XTick',[-8 0 8],'YTick',[-8 0 8], ...
 		   'Drawmode','fast','Visible','on','NextPlot','add');
@@ -79,32 +79,32 @@ class tacoma:
 		  set(rcable,'xdata',[c c],'ydata',[s-y(1,1) 8])
 		  drawnow; pause(h)
 
-	  	"""
+		"""
 
-	  	# TODO: Set up plot
-	  	# t(1)=inter(1);
+		# TODO: Set up plot
+		# t(1)=inter(1);
 		#set(gca,'XLim',[-8 8],'YLim',[-8 8], ...
-	   	#	'XTick',[-8 0 8],'YTick',[-8 0 8], ...
-	   	#	'Drawmode','fast','Visible','on','NextPlot','add');
+		#	'XTick',[-8 0 8],'YTick',[-8 0 8], ...
+		#	'Drawmode','fast','Visible','on','NextPlot','add');
 		#cla;                                 % clear screen
 		#axis square                          % make aspect ratio 1 - 1
 		#road=line('color','b','LineStyle','-','LineWidth',5,...
-		#    'erase','xor','xdata',[],'ydata',[]);
+		#	'erase','xor','xdata',[],'ydata',[]);
 		#lcable=line('color','r','LineStyle','-','LineWidth',1,...
-		#    'erase','xor','xdata',[],'ydata',[]);
+		#	'erase','xor','xdata',[],'ydata',[]);
 		#rcable=line('color','r','LineStyle','-','LineWidth',1,...
-		#    'erase','xor','xdata',[],'ydata',[]);
+		#	'erase','xor','xdata',[],'ydata',[]);
 
-	  	z1 = []
-	  	z3 = []
+		z1 = []
+		z3 = []
 		for k in range(0, self.n-1):
-		  	for i in range(0, self.p):
-		  		self.t[i+1] = self.t[i]+h;
-		  		for j in range(self.y[i]):
-		  			self.y[i+1][j] = self.trapstep(self.t[i],self.y[i][j], self.h) # might be incorrect -> maybe self.y[i] (entire row) is passed?
-		  	for i in range(self.y[i]):
-  				self.y[0][i] = self.y[self.p][i]
-  				self.t[0] = self.t[self.p]
+			for i in range(0, self.p):
+				self.t[i+1] = self.t[i]+h;
+				for j in range(self.y[i]):
+					self.y[i+1][j] = self.trapstep(self.t[i],self.y[i][j], self.h) # might be incorrect -> maybe self.y[i] (entire row) is passed?
+			for i in range(self.y[i]):
+				self.y[0][i] = self.y[self.p][i]
+				self.t[0] = self.t[self.p]
 			z1.append(self.y[0][0])
 			z3.append(self.y[0][2])
 			c = self.len*math.cos(self.y[0][2])
